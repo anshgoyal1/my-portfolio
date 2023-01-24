6542194e4a07d94c372ad0f3938e64bbcd6127fc
Ansh goyal <anshgoyal749@gmail.com>
2023-01-24 23:23:38 +0530

    i

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -2,10 +2,12 @@ import React, { useContext } from "react";
 import "./Portfolio.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import Sidebar from "../../img/sidebar.png";
-import Ecommerce from "../../img/ecommerce.png";
-import HOC from "../../img/hoc.png";
-import MusicApp from "../../img/musicapp.png";
+import portfolio from "../../img/portfolio.png";
+import instagram from "../../img/instagram.jpeg";
+import diabetes from "../../img/diabetes.jpg";
+import facemask from "../../img/facemask.png";
+import chatapp from "../../img/chatapp.jpg";
+import winequality from "../../img/winequality.png";
 import { themeContext } from "../../Context";
 
 const Portfolio = () => {
@@ -25,19 +27,22 @@ const Portfolio = () => {
         className="portfolio-slider"
       >
         <SwiperSlide>
-          <img src={Sidebar} alt="" />
+          <img src={portfolio} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={Ecommerce} alt="" />
+          <img src={instagram} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={MusicApp} alt="" />
+          <img src={facemask} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={HOC} alt="" />
+          <img src={diabetes} alt="" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={HOC} alt="" />
+          <img src={chatapp} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={winequality} alt="" />
         </SwiperSlide>
       </Swiper>
     </div>
